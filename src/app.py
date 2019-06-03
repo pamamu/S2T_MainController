9c9cc5752e77ee952a51e9be2bd4b1f961de33ba
Pablo Macias <pablomaciasmu@gmail.com>
2019-06-03 18:30:56 +0200

    Handler Error Fixed

--- src/app.py
+++ src/app.py
@@ -10,19 +10,20 @@ if __name__ == '__main__':
     if len(sys.argv) < 3:
         print("Insert shared folder path + containers list")
         sys.exit(1)
-
     args = sys.argv
     print(args)
 
     base_path = args[1]
     list_path = args[2]
 
+    handler = None
     if os.path.isdir(base_path) and os.path.isfile(list_path):
         containers_list = json.loads(open(list_path).read())
     else:
         print("Shared Folder Path and Containers List INVALID")
         sys.exit(2)
     try:
+        print("OK")
         reset_containers()
         handler = MainHandler(base_path, containers_list)
         handler.start()
@@ -33,5 +34,6 @@ if __name__ == '__main__':
     except Exception as e:
         print(e)
     finally:
-        handler.stop()
-        print(handler.thread.isAlive())
+        if handler is not None:
+            handler.stop()
+            print(handler.thread.isAlive())
